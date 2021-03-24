dd2b3cd64f00f9ca2ca26e1c0881d04ea7d8abda
Sanaullah-sarazai <74836459+Sanaullah-sarazai@users.noreply.github.com>
2021-03-25 02:10:33 +0500

    Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home  from './components/pages/Home';
+import Reports from './components/pages/Reports';
+import Products from './components/pages/Products';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
